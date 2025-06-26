c34e45fde3e546d5888a5e6c446215c7b9873620
Sanskar Jain <sanskarj257@gmail.com>
2025-06-26 23:04:37 +0530

    Basic module

---
+++ Basics/script.js
@@ -0,0 +1,42 @@
+// FUNCTIONS IN JAVASCRIPT
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(4, 5));
+
+// let result = sum(4, 5);
+// console.log(result);
+
+
+// OBJECTS IN JAVASCRIPT
+
+// const person = {
+//     firstName: "Sanskar",
+//     lastName: "J",
+//     age: 24,
+//     eyeColor: "brown",
+//     fullName: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// console.log(person);
+
+// Accessing object properties
+
+// console.log(person.firstName);
+// console.log(person["age"]);
+
+// Mutating object properties
+
+// person.age = 25;
+// console.log(person.age);
+
+// const newPerson = person;
+// newPerson.age = 24;
+// console.log(newPerson);
+// console.log(person);
+
+// console.log(person.fullName());
